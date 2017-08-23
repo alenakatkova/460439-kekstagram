d7a08e4bd6d57a358432c67cf7ae4e91b315854c
lalalakolpachok <akolpakova91@gmail.com>
2017-08-23 20:42:25 +0300

    + еще одна функция

--- js/pictures.js
+++ js/pictures.js
@@ -1,28 +1,33 @@
 'use strict';
 var pictures = [];
-var commentsList = [
-  'Всё отлично!',
-  'В целом всё неплохо. Но не всё.',
-  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
-  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
-  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
-];
 
 function generateRandomInteger(min, max) {  // generating random whole number in the range min-max
   return Math.floor(min + Math.random() * (max + 1 - min));
 }
 
+function getPictureUrl(index) {
+  return 'photos/' + index + '.jpg';
+}
+
 function generatePictures(amountOfPictures) {  // generating an array of pictures with URL, random number of likes & random comments
   var picture;
+  var comments = [
+    'Всё отлично!',
+    'В целом всё неплохо. Но не всё.',
+    'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+    'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+    'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+    'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+  ];
+
   for (var i = 1; i <= amountOfPictures; i++) {
     picture = {
-      url: 'photos/' + i + '.jpg',
+      url: getPictureUrl(i),
       likes: generateRandomInteger(15, 250),
       comments: []
     };
     for (var j = 0; j <= generateRandomInteger(0, 1); j++) {
-      picture.comments.push(commentsList[Math.floor(Math.random() * 6)]);
+      picture.comments.push(comments[Math.floor(Math.random() * comments.length)]);
     }
     pictures.push(picture);
   }
